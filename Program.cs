91da71b6854c5e10a3986058032d90e49f6f7694
LazyLoler <lazyloler@gmail.com>
2026-05-18 20:08:33 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new HttpClient();
+        Console.WriteLine("Enter an artist: ");
+        string? artist = Console.ReadLine();
+        string? title = Console.ReadLine();
+        string url = $"https://api.lyrics.ovh/v1/{artist}/{title}";
+
+        HttpResponseMessage response = await client.GetAsync(url);
+        string result = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(result);
+    }
+}
